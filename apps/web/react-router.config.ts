fbaea2173d02debc3b51905c70c6d66845d8f7bc
Gokulnath8887 <gokul9g0t@gmail.com>
2026-02-12 23:01:30 +0530

    fix(restructure): move web app to root and simplify deployment structure

--- apps/web/react-router.config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-	appDirectory: './src/app',
-	ssr: true,
-	prerender: ['/'],
-} satisfies Config;
-
---
+++ react-router.config.ts
@@ -0,0 +1,6 @@
+export default {
+	appDirectory: './src/app',
+	ssr: true,
+	prerender: ['/'],
+} satisfies Config;
+
